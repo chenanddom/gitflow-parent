12cddc7956d1c82c8f292df4cff53c397ea34248
chend <chend@seirobotics.net>
2020-12-01 14:05:31 +0800

    提交代码222

--- src/main/java/com/itdom/Demo.java
+++ src/main/java/com/itdom/Demo.java
@@ -3,5 +3,6 @@ package com.itdom;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Demo");
+        System.out.println("Hello Demo222");
     }
 }
