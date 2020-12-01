2d396803afead627c7f1f18e31f41e276b489444
chend <chend@seirobotics.net>
2020-12-01 14:17:48 +0800

    提交代码...

--- src/main/java/com/itdom/Demo.java
+++ src/main/java/com/itdom/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Demo");
         System.out.println("Hello Demo222");
+        System.out.println("Hello Demo2223");
     }
 }
